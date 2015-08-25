54a673802227518f8284c03f8d697f91641abb2b
SivoP <siv.padhy@gmail.com>
2015-08-25 14:52:35 -0700

    basic HTTP requests to Spotify setup

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'httparty'
 gem 'rake'
 gem 'activesupport'
 
--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,17 @@
 # Homepage (Root path)
+
+
 get '/' do
   erb :index
+end
+
+
+get '/login' do
+ reponse = HTTParty.get("https://accounts.spotify.com/authorize/?client_id=0422daa52d9c46f48d3ff2a0edfbc71a&response_type=code")
+
+ puts response.body
+end
+
+get '/hello' do
+  erb :hello
 end
