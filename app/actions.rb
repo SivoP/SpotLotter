d7c5f169c8513609ab3a402a04731c5196a3a19d
SivoP <siv.padhy@gmail.com>
2015-08-25 16:03:18 -0700

    app properly hits API endpoint for user to login

--- app/actions.rb
+++ app/actions.rb
@@ -7,9 +7,8 @@ end
 
 
 get '/login' do
- reponse = HTTParty.get("https://accounts.spotify.com/authorize/?client_id=0422daa52d9c46f48d3ff2a0edfbc71a&response_type=code")
+ HTTParty.get("https://accounts.spotify.com/authorize/?client_id=62ccc5854bf848bc94e84215ee0558d2&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A9393%2Fhello&scope=user-read-private%20user-read-email").to_s
 
- puts response.body
 end
 
 get '/hello' do
