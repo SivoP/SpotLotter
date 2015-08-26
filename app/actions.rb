11631df84193d26932ac008faf9a631dad009bc2
SivoP <siv.padhy@gmail.com>
2015-08-26 11:00:40 -0700

    authentication all wired up, can start making API calls for data

--- app/actions.rb
+++ app/actions.rb
@@ -11,12 +11,9 @@ get '/login' do
 end
 
 get '/hello' do
-  # trying to put client id and secret into post request body, might not work
-
   client_id = ENV["CLIENT_ID"]
   client_secret = ENV["CLIENT_SECRET"]
   client_id_and_secret = Base64.strict_encode64("#{client_id}:#{client_secret}")
-
   response = HTTParty.post("https://accounts.spotify.com/api/token",
   :body => {
     :grant_type => "authorization_code",
@@ -25,19 +22,8 @@ get '/hello' do
   },
   :headers => {
     "Authorization" => "Basic #{client_id_and_secret}"
-  })
-  byebug
-  # BELOW: original attempt with all parameters directly in the URL
-  # response = HTTParty.post("https://accounts.spotify.com/api/token/?grant_type=authorization_code&code=#{params[:code]}0&redirect_uri=http%3A%2F%2Flocalhost%3A9393%2Fhello",
-  #  {
-  #   headers: {
-  #     'Authorization'=> 'Basic '+client_id+':'+client_secret
-  #   }
-  #  }
-  # )
-  
-
-  
+  }).to_hash
+  erb :hello
 end
 
 
